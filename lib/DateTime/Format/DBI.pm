840fd8b708cd4928fbb52bc999ea9d2ba8a5b9ba
Claus Färber <CFAERBER@cpan.org>
2003-07-20 15:13:19 +0000

    Import of CFAERBER/DateTime-Format-DBI-0.03 from CPAN.
    gitpan-cpan-distribution: DateTime-Format-DBI
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         CFAERBER/DateTime-Format-DBI-0.03.tar.gz
    gitpan-cpan-author:       CFAERBER
    gitpan-cpan-maturity:     released

--- lib/DateTime/Format/DBI.pm
+++ lib/DateTime/Format/DBI.pm
@@ -1,29 +1,31 @@
 package DateTime::Format::DBI;
-# $Id: Pg.pm,v 1.6 2003/05/30 14:14:23 cfaerber Exp $
+# $Id: DBI.pm,v 1.2 2003/07/20 14:17:27 cfaerber Exp $
 
 use strict;
 use vars qw ($VERSION);
 
 use Carp;
-use DateTime 0.10;
+use DBI 1.21;
 
-$VERSION = '0.02';
-
-$VERSION = eval $VERSION;
+$VERSION = '0.03';
+$VERSION = $VERSION + 0.0;
 
 our %db_to_parser = (
+  # lowercase for case-insensitivity!
   'mysql'	=> 'DateTime::Format::MySQL',
-  'Pg'		=> 'DateTime::Format::Pg',
+  'pg'		=> 'DateTime::Format::Pg',
 );
 
 sub new {
   my ($name,$dbh) = @_;
-
   UNIVERSAL::isa($dbh,'DBI::db') || croak('Not a DBI handle.');
-  my $dbtype = $dbh->{Driver}->{Name};
 
-  my $pclass = $db_to_parser{$dbtype};
-  $pclass || croak("Unsupported database driver 'DBD::".$dbtype."'");
+# my $dbtype = $dbh->{Driver}->{Name};
+  my @dbtypes = DBI::_dbtype_names($dbh);
+  my $dbtype = shift @dbtypes;
+
+  my $pclass = $db_to_parser{lc $dbtype};
+  $pclass || croak("Unsupported database driver '".$dbtype."'");
 
   my $parser = eval "use $pclass; $pclass->new();";
 
@@ -54,7 +56,9 @@ DateTime::Format::DBI - Find a parser class for a database connection.
 This module finds a C<DateTime::Format::*> class that is suitable for the use with
 a given DBI connection (and C<DBD::*> driver).
 
-It currently supports the following drivers: MySQL, PostgreSQL (Pg).
+It currently supports the following drivers: 
+L<MySQL|DateTime::Format::MySQL>, 
+L<PostgreSQL (Pg)|DateTime::Format::Pg>.
 
 =head1 CLASS METHODS
 
@@ -64,6 +68,65 @@ It currently supports the following drivers: MySQL, PostgreSQL (Pg).
 
 Creates a new C<DateTime::Format::*> instance the exact class of which depends
 on the driver used for the database connection referenced by $dbh. 
+
+=back
+
+=head1 PARSER/FORMATTER INTERFACE
+
+C<DateTime::Format::DBI> is just a front-end factory that will return one of
+the format classes based on the nature of your $dbh.
+
+For information on the interface of the returned parser object, please see the
+documentation for the class pertaining to your particular $dbh.
+
+In general, parser classes for databases will implement the following methods.
+For more information on the exact behaviour of these methods, see the
+documentation of the parser class.
+
+=over 4
+
+=item * parse_datetime( $string )
+
+Given a string containing a date and/or time representation from
+the database used, this method will return a new C<DateTime>
+object.
+
+If given an improperly formatted string, this method may die.
+
+=item * format_datetime( $dt )
+
+Given a C<DateTime> object, this method returns a string
+appropriate as input for all or the most common date and date/time
+types of the database used. 
+
+=item * parse_duration( $string )
+
+Given a string containing a duration representation from the
+database used, this method will return a new C<DateTime::Duration>
+object.
+
+If given an improperly formatted string, this method may die.
+
+Not all databases and format/formatter classes support durations;
+please use L<UNIVERSAL::has|UNIVERSAL/has> to check for the
+availability of this method.
+
+=item * format_duration( $du )
+
+Given a C<DateTime::Duration> object, this method returns a string
+appropriate as input for the duration or interval type of the
+database used.
+
+Not all databases and parser/formatter classes support durations;
+please use L<UNIVERSAL::has|UNIVERSAL/has> to check for the
+availability of this method.
+
+=back
+
+Parser/formatter classes may additionally define methods like
+parse_I<type> or format_I<type> (where I<type> is derived from the
+SQL type); please see the documentation of the individual format
+class for more information.
 
 =head1 SUPPORT
 
@@ -86,7 +149,7 @@ this module.
 
 =head1 SEE ALSO
 
-L<DateTime::Format::MySQL>, L<DateTime::Format::Pg>
+L<DateTime>, L<DBI>
 
 datetime@perl.org mailing list
 
--- t/1basic.t
+++ t/1basic.t
@@ -1,4 +1,4 @@
-# $Id: 1basic.t,v 1.2 2003/05/30 14:04:49 cfaerber Exp $
+# $Id: 1basic.t,v 1.1 2003/06/16 15:06:05 cfaerber Exp $
 use Test::More tests => 1;
 BEGIN { 
   use_ok('DateTime::Format::DBI')
